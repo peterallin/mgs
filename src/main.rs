598313c30845ca53836fd067eb05389c3e4cd837
Peter Allin <peter@peca.dk>
2020-10-25 16:33:46 +0100

    Correct the call to with_context

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use anyhow::Context;
 use std::path::PathBuf;
 use structopt::StructOpt;
-use anyhow::Context;
 
 mod output;
 mod repos;
@@ -13,9 +13,10 @@ struct Options {
 
 fn main() -> anyhow::Result<()> {
     #[cfg(target_os = "windows")]
-    ansi_term::enable_ansi_support().with_context("Failed to enable ANSI color support")?;
+    ansi_term::enable_ansi_support().with_context(|| "Failed to enable ANSI color support")?;
 
     let options = Options::from_args();
-    output::print_changed(&options.top_dir).with_context(|| format!("Failed to print changes of {}", options.top_dir.display()))?;
+    output::print_changed(&options.top_dir)
+        .with_context(|| format!("Failed to print changes of {}", options.top_dir.display()))?;
     Ok(())
 }
